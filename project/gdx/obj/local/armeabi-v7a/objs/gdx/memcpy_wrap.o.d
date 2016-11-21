8de107f9672ab907bef1f62c24275ce541dc3422
kongyatong <kongyatong@doodlemobile.com>
2016-11-21 13:45:05 +0800

    重新配置工程

--- project/gdx/obj/local/armeabi-v7a/objs/gdx/memcpy_wrap.o.d
+++
@@ -1,2 +0,0 @@
-/cygdrive/d/libgdx-0.9.9/gdx/obj/local/armeabi-v7a/objs/gdx/memcpy_wrap.o: \
- /cygdrive/d/libgdx-0.9.9/gdx/jni/memcpy_wrap.c
--- project/gdx/obj/local/x86/objs/gdx/memcpy_wrap.o.d
+++
@@ -1,2 +0,0 @@
-/cygdrive/d/libgdx-0.9.9/gdx/obj/local/x86/objs/gdx/memcpy_wrap.o: \
- /cygdrive/d/libgdx-0.9.9/gdx/jni/memcpy_wrap.c
